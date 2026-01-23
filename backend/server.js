d55283f49a93b1348b6d39668cffb383bdcea7ad
Ashish <ashish.a.dikshit@gmail.com>
2026-01-23 16:16:16 +0530

    updated port setup in server js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,11 @@
 import app from "./app.js";
-console.log(app);
+import dotenv from "dotenv";
+// const PORT = process.env.PORT || 5000;
+
+dotenv.config({ path: "backend/config/config.env" });
 
 const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
